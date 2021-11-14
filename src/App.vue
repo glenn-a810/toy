5ddfbc64ba4c341ba3daf1972cafee69600d54d5
Glenn <a810.glenn@gmail.com>
2021-11-15 00:02:54 +0900

    font change (한글폰트 귀찮..)

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,16 @@
   <router-view />
 </template>
 <script>
-import { defineComponent } from 'vue';
+import { defineComponent } from 'vue'
 
 export default defineComponent({
-  name: 'App'
+  name: 'App',
 })
 </script>
+<style>
+body {
+  font-family: Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto,
+    'Helvetica Neue', 'Segoe UI', 'Apple SD Gothic Neo', 'Noto Sans KR',
+    'Malgun Gothic', sans-serif;
+}
+</style>
