8e9805a77578dcc4f1fd9c1e50a2550ef6b7b1fb
Glenn <a810.glenn@gmail.com>
2021-11-20 22:27:06 +0900

    test image 출력. <q-img>랑 <img>의 차이 알고있어야함.

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,10 +1,12 @@
 <template>
   <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+<!--    <img-->
+<!--      alt="Quasar logo"-->
+<!--      src="~assets/quasar-logo-vertical.svg"-->
+<!--      style="width: 200px; height: 200px"-->
+<!--    >-->
+<!--    <q-img src="~assets/img_1v.JPG" :ratio="1" />-->
+<!--    <img src="~assets/img_1v.JPG" alt="Vertical Image" />-->
   </q-page>
 </template>
 
