d3b80a0c158ac08f1657d24a4f209209b6aac87e
Glenn <a810.glenn@gmail.com>
2021-11-25 22:18:15 +0900

    일단 array에 강제로 넣어서 되는지 확인

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -2,24 +2,32 @@
   <q-page class="flex flex-center">
     <q-img
       src="~assets/img_1.jpg"
-      style="max-width: 500px; max-height: 500px;"
+      style="max-width: 500px; max-height: 500px"
       fit="scale-down"
     />
     <q-img
       src="~assets/img_1.jpg"
-      style="max-width: 500px; max-height: 500px;"
+      style="max-width: 500px; max-height: 500px"
       fit="scale-down"
     />
   </q-page>
   <div>
-    <p>선택한 이미지 : </p>
+    <p>선택한 이미지 :</p>
   </div>
 </template>
 
 <script>
 import { defineComponent } from 'vue'
+import { date } from 'quasar'
 
 export default defineComponent({
   name: 'PageIndex',
+  date: () => [
+    {
+      id: 1,
+      select: false,
+      imageName: '',
+    },
+  ],
 })
 </script>
