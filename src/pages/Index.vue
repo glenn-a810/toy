e6ed702e505b1bdae9ba3877c34e8b0e465956ab
Glenn <a810.glenn@gmail.com>
2021-12-02 22:36:48 +0900

    이미지 선택은 뭘로 해야하지..

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,25 +1,26 @@
 <template>
-  <q-page class="full-width row justify-start items-center content-start">
+  <q-page class="full-width row items-center content-start q-pa-xs">
     <q-img
-      class="col-4 col-md-6"
+      class="col-6"
       src="~assets/img_1.jpg"
-      style="max-width: 50%; max-height: 50%; overflow: auto; background: red"
+      style="max-width: 100%; max-height: 100%; overflow: auto; background: red"
       fit="scale-down"
     />
     <q-img
-      class="col-4 col-md-6"
+      class="col-6"
       src="~assets/img_1.jpg"
-      style="max-width: 50%; max-height: 50%; overflow: auto; background: blue"
+      style="
+        max-width: 100%;
+        max-height: 100%;
+        overflow: auto;
+        background: blue;
+      "
       fit="scale-down"
     />
-    <div v-if="imageSelected" class="col-12" style="background: red">
+    <div v-if="imageSelected" class="q-pa-md q-gutter-x-md">
       <q-btn icon="keyboard_arrow_left" label="이전" color="primary" />
-      <q-btn
-        icon-right="keyboard_arrow_right"
-        label="다음"
-        color="primary"
-        disable
-      />
+      <q-btn icon-right="keyboard_arrow_right" label="다음" color="primary" />
+      <p>선택한 이미지 : {{ imageSelected }}</p>
     </div>
   </q-page>
 </template>
